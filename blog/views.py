cf994fe816ad074bbb8b0dfe3b62966d2f438ba5
shen lee <shenleekhalid@gmail.com>
2025-07-19 15:19:17 +0300

    trigram similarity/weighting queries/stemming and ranking

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,9 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 class PostListView(ListView):
@@ -107,13 +109,34 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
+
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+
+            search_vector = (
+                SearchVector('title', weight='A') +
+                SearchVector('body', weight='B', config='spanish')
+            )
+            search_query = SearchQuery(query, config='spanish')
+
+            # Try full-text search first
             results = Post.published.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(
+                rank__gte=0.3
+            ).order_by('-rank')
+
+            # If no good full-text results, use Trigram similarity fallback
+            if not results:
+                results = Post.published.annotate(
+                    similarity=TrigramSimilarity('title', query)
+                ).filter(
+                    similarity__gte=0.2
+                ).order_by('-similarity')
+
     return render(request, "blog/post/search.html", {
         "form": form,
         "query": query,
